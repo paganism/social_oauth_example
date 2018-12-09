a63aa013daff73c7609f2945b5198ef410871d4f
YuriyB <paganism@mail.ru>
2018-12-09 22:31:31 +0300

    added constant vk_api_version

--- soauth_example/soauth/views.py
+++ soauth_example/soauth/views.py
@@ -6,6 +6,7 @@ from social_django.models import UserSocialAuth
 
 import vk
 
+VK_API_VERSION = '5.92'
 
 def index(request):
     friends_data = []
@@ -13,7 +14,7 @@ def index(request):
         active_user = get_object_or_404(User, username=request.user)
         soauth_user = get_object_or_404(UserSocialAuth, user_id=active_user)
         session = vk.Session(access_token=soauth_user.access_token)
-        api = vk.API(session, v='5.92')
+        api = vk.API(session, v=VK_API_VERSION)
         friends_list = (api.friends.get(user_id=soauth_user.uid)['items'])[:5]
         for i in friends_list:
             friends_data.append(api.users.get(user_ids=i, fields=('screen_name', 'city', 'country', 'photo_200')))
